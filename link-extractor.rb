1f0f999e905fe9ba4d3538f020e0521c402f4743
rob <rob@studiocenter.com>
2014-07-03 12:21:57 -0400

    cleaned up code a little more

--- link-extractor.rb
+++ link-extractor.rb
@@ -11,6 +11,7 @@ class LinkScrapper
 
 		# init link store hashes
 		@search_index = 0
+		@search_iteration = 0
 		@links = Array.new
 		@checked_links = Hash.new
 		@error_links = Hash.new
@@ -31,77 +32,77 @@ class LinkScrapper
 
 	end
 
+	# gather search uri
+	def get_search_uri
+
+		if @search_iteration > 0
+			# set search uri
+			if !@links[@search_index].nil?
+				@search_uri = @links[@search_index][0].chomp
+			else
+				# save results and exit
+				save_results
+				exit
+			end
+			# check for existing link check data
+			# check for direct link
+			if @search_uri[0,5] == "http:" || @search_uri[0,6] == "https:"
+				# if external link go to next link
+				if @search_uri.index(@local_domain[0]) == nil
+					@external_links.push(@search_uri)
+					@skip = 1	
+				end
+				# increment search index value
+				@search_index += 1
+			else
+				# check for mailto link
+				if @search_uri[0,7] == "mailto:" || @search_uri[0,4] == "tel:"
+					@skip = 1
+				else
+					# check for relative link
+					if @search_uri[0,2] != "//" && @search_uri[0] == "/"
+						@search_uri[0] = ""
+					end
+					# define uri string
+					if @search_uri[0,2] != "//"
+						@search_uri = "#{@search_domain}#{@search_uri}"
+					else
+						# handle protocol agnotic link requests
+						if @search_domain[0,6] == "https:"
+							@search_uri = "https:#{@search_uri}"
+						else
+							@search_uri = "http:#{@search_uri}"
+						end
+					end
+				end
+				# increment search index value
+				@search_index += 1
+			end
+		end
+	end
+
 	# gather link data
 	def get_links
 
-		search_uri = ""
-		skip = 0
+		# init skip bit
+		@skip = 0
 
 		# define search uri if undefined
-		if search_uri == ""
-			if @search_uri != ""
-				search_uri = @search_uri
-				# empty default @search_uri once used
-				@search_uri = ""
-			else
-				# set search uri
-				if !@links[@search_index].nil?
-					search_uri = @links[@search_index][0]
-				else
-					# save results and exit
-					save_results
-					return
-				end
-				# check for existing link check data
-				# check for direct link
-				if search_uri[0,5] == "http:" || search_uri[0,6] == "https:"
-					# if external link go to next link
-					if search_uri.index(@local_domain[0]) == nil
-						@external_links.push(search_uri)
-						skip = 1	
-					end
-					# increment search index value
-					@search_index += 1
-				else
-					# check for mailto link
-					if search_uri[0,7] == "mailto:" && search_uri[0,4] == "tel:"
-						skip = 1
-					else
-						# check for relative link
-						if search_uri[0,2] != "//" && search_uri[0] == "/"
-							search_uri[0] = ""
-						end
-						# define uri string
-						if search_uri[0,2] != "//"
-							search_uri = "#{@search_domain}#{search_uri}"
-						else
-							# handle protocol agnotic link requests
-							if @search_domain[0,6] == "https:"
-								search_uri = "https:#{search_uri}"
-							else
-								search_uri = "http:#{search_uri}"
-							end
-						end
-					end
-					# increment search index value
-					@search_index += 1
-				end
-			end
-		end
+		get_search_uri
 
 		# check for existing uri hash index
-		if @checked_links[search_uri.to_sym]
-			skip = 1
+		if @checked_links[@search_uri.to_sym]
+			@skip = 1
 		end
 
-		# run link scan if skip bit is not set
-		if skip == 0
+		# run link scan if @skip bit is not set
+		if @skip == 0
 
 			# let user know which uri is currently active
-			puts search_uri
+			puts @search_uri
 
 			# gather page request response
-			response = Net::HTTP.get_response(URI.parse(search_uri))
+			response = Net::HTTP.get_response(URI.parse(@search_uri))
 
 			# store response page body
 			body = response.body
@@ -114,8 +115,8 @@ class LinkScrapper
 
 			# update anchors and indirect links to use direct links
 			links_array.each { |val|
-				if val[0,7] != "mailto:" && val[0,4] != "tel:" && val[0] != "/" && val[0,5] != "http:" && val[0,6] != "https:" && val[0,2] != "//"
-					val = "#{search_uri}#{val}"
+				if val[0] != "/" || val[0,5] != "http:" || val[0,6] != "https:" || val[0,2] != "//"
+					val = "#{@search_uri}#{val}"
 				end
 			}
 
@@ -126,17 +127,19 @@ class LinkScrapper
 			@links.uniq!
 
 			# store results in checked hash
-			@checked_links[search_uri.to_sym] = code
+			@checked_links[@search_uri.to_sym] = code
 
 		end
 
 		# iterate through found links
+		@search_iteration += 1
 		get_links
 
 	end
 
+	# save results to csvs
 	def save_results
-		# save results
+		# save search results
 		CSV.open("results.csv", "wb") {|csv| 
 			@checked_links.each {|key| 
 				csv << key
